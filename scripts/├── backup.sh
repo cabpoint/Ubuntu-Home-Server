5efbebd7538dbdab76efe001c4a8c673dc535952
cabpoint <155767678+cabpoint@users.noreply.github.com>
2026-07-29 02:06:23 +0200

    Update ├── backup.sh

--- scripts/├── backup.sh
+++ scripts/├── backup.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+
+BACKUP_DIR="/opt/backups"
+
+mkdir -p "$BACKUP_DIR"
+
+cp -r /etc/nginx "$BACKUP_DIR"
+
+cp /etc/ssh/sshd_config "$BACKUP_DIR"
+
+cp /etc/netplan/00-installer-config.yaml "$BACKUP_DIR"
+
+tar -czf "$BACKUP_DIR/server-backup.tar.gz" "$BACKUP_DIR"
+
+echo
+echo "Backup completed successfully."
